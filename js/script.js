9f5cd89a606f57f7f06e42fb05a6794e7db58dac
Debi Prasad Swain <debiprasadswain06@gmail.com>
2025-07-11 20:23:05 +0530

    Initial commit - KRISHAN project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// let searchBtn = document.querySelector('#searchicon');
+// let searchBar= document.querySelector('.searchbarcontainer');
+// window.onscroll = () =>{
+//     searchBtn.classList.remove('fa-times');
+//     searchBar.classList.remove('active');
+//     menuBar.classList.remove('fa-times')
+
+// }
+// searchBtn.addEventListener('click', () =>{
+//     searchBtn.classList.toggle('fa-times');
+//     searchBar.classList.toggle('active');
+// });
+
+
+
+
+
